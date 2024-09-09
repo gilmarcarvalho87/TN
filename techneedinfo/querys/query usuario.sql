f7ddc7c4a3058907f5d356372346f423e67194c9
gilmar <gilmarpcarvalho@gmail.com>
2024-09-09 09:23:27 -0300

    09-09-24

--- techneedinfo/querys/query usuario.sql
+++ techneedinfo/querys/query usuario.sql
@@ -6,6 +6,6 @@ create table usuarios_autenticados(
 	id int not null primary key AUTO_INCREMENT,
 	nome varchar(100) not null,
 	email varchar(150) not null,
-	senha varchar(32) not null
+	senha varchar(32) not null,
 	ativo BIT(1) NOT NULL DEFAULT b'1',
 );
